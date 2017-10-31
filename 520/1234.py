ccb9d27c4863c1080a69d45fea1a23cd840142a1
dmart030 <dmart030@ucr.edu>
2017-10-30 20:24:35 -0700

    10/30/2017
    Uploaded two files to visualized some sort of orbit around a planet. I'm not  sure but this regarding the centre of mass.  Will update another date.

---
+++ 520/1234.py
@@ -0,0 +1,17 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import animation
+
+x = y = z =[]
+csv = np.genfromtxt('horizons_results_ssbWRTsuncenter.csv', delimiter=",")
+X = csv[:,2]
+Y = csv[:,3]
+Z = csv[:,4]
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.plot(X, Y, Z, label='Horizon_results')
+ax.legend()
+plt.show()
